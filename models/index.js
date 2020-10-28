4322897bdd9e63dd666439edd586834d7cbd3326
Garlic Fiend <suzannaakins@gmail.com>
2020-10-28 02:35:59 -0700

    some associations made, category-routes written

--- models/index.js
+++ models/index.js
@@ -4,13 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category - one-to-many relationship between category + products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: '?',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: '?',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
